aaf36ee826893c9dbd8acf903636046814e194ee
Winston Saldanha <86848649+thewinstonsaldanha@users.noreply.github.com>
2022-10-30 15:14:08 +0530

    Time: 27 ms (44.37%), Space: 52.1 MB (27.23%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
+        if(ransomNote.length()>magazine.length()){
+            return false;
+        }
         HashMap<Character, Integer> counts= new HashMap<>();
         
         for(char c: magazine.toCharArray()){
