9308bb0e60d6d22a77d0d3469b38b7b870b01da9
Winston Saldanha <86848649+thewinstonsaldanha@users.noreply.github.com>
2022-11-03 17:34:33 +0530

    Time: 35 ms (86.17%), Space: 95.8 MB (78.54%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestPalindrome(String[] words) {
+        int result=0;
+        int freq[][]= new int [26][26];
+        
+        for(String s: words){
+            int i= s.charAt(0)- 'a';
+            int j= s.charAt(1)- 'a';
+            if(freq[j][i]> 0){
+                result= result+4;
+                freq[j][i]--;
+            }
+            else{
+            freq[i][j]++;   
+            }
+        }
+        
+        
+        for(int i=0; i<26; i++){
+            if(freq[i][i]>0){
+                result= result+2;
+                break;
+            }
+            
+        }
+        return result;
+        
+    }
+}
