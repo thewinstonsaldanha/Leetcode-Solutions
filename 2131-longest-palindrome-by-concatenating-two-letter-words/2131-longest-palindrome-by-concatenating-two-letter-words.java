16edf5d9803549498e1567f28575c1657f805f9c
Winston Saldanha <86848649+thewinstonsaldanha@users.noreply.github.com>
2022-11-03 17:40:19 +0530

    Time: 15 ms (98.38%), Space: 57.8 MB (94.64%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,25 +1,25 @@
 class Solution {
     public int longestPalindrome(String[] words) {
         int result=0;
-        int freq[][]= new int [26][26];
+        int freq[][]= new int [26][26];  //initialized array
         
         for(String s: words){
-            int i= s.charAt(0)- 'a';
-            int j= s.charAt(1)- 'a';
-            if(freq[j][i]> 0){
-                result= result+4;
-                freq[j][i]--;
+            int i= s.charAt(0)- 'a';  //extracted first element
+            int j= s.charAt(1)- 'a';  //extracted second element
+            if(freq[j][i]> 0){        //if multiples are found so they are palindrome
+                result= result+4;     // if found then add 4
+                freq[j][i]--;         //keep only one discard the other
             }
             else{
-            freq[i][j]++;   
+            freq[i][j]++;            //else keep a count
             }
         }
         
         
         for(int i=0; i<26; i++){
-            if(freq[i][i]>0){
-                result= result+2;
-                break;
+            if(freq[i][i]>0){        //if similar alphabets in word are found e.g. gg
+                result= result+2;    // add their result to 2
+                break;               // break then 
             }
             
         }
