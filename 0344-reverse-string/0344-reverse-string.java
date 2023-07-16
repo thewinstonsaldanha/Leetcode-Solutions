2e2eb7e908abfae1164b03e6aba2dd42168924b5
Winston Saldanha <86848649+thewinstonsaldanha@users.noreply.github.com>
2023-07-16 10:37:27 +0530

    Time: 1 ms (82.65%), Space: 51.6 MB (6.09%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int left=0;
+        int right= s.length -1 ;
+        
+        while(left<right){
+            char c= s[left];
+            s[left]= s[right];
+            s[right]=  c;
+            
+            left= left+1;
+            right= right-1;
+        }
+        
+    }
+}
